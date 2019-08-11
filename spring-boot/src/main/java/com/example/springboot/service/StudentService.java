75e0d621df78628f9f9e37ce3f32d990f33f41bd
jc7213 <30779167@qq.com>
2019-08-11 21:05:20 +0800

    abc

--- spring-boot/src/main/java/com/example/springboot/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.service;
-
-import com.example.springboot.entity.Student;
-
-public interface StudentService {
-    public void register(Student stu);
-    public int findStudentCount();
-    public Student findById(int id);
-}
